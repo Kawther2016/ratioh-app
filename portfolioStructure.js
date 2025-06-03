557be33a7b387e0993afcc8cb6a913b7fb7688ee
Kawther2016 <kawtharalnasry@gmail.com>
2025-06-03 22:00:29 +0400

    RD-121 Define basic portfolio data structure

---
+++ portfolioStructure.js
@@ -0,0 +1,11 @@
+// RD-121 Design Data Structure to Store User Portfolios
+
+const portfolios = [
+  {
+    id: 1,
+    name: "Retirement Fund",
+    description: "Long-term investments",
+    createdAt: "2025-05-26",
+    assets: [],
+  },
+];
